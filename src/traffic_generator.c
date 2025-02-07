97dba3c53cb1a399f682bb88e0502e128b4bbc47
salina-nakarmi <salina123karmi@gmail.com>
2025-02-07 17:58:47 +0545

    modified random vehicaL number genreator function for more realistic vehicle numbers

---
+++ src/traffic_generator.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+
+//#define SERVER_IP "0.0.0.0" // for all network available
+#define SERVER_IP "127.0.0.1" //local host
+#define PORT 5000
+#define BUFFER_SIZE 100
+
+// Generate a random vehicle number
+void generateVehicleNumber(char* buffer) {
+    static const char* states[] = {"KA", "MH", "DL", "UP", "TN"};
+    sprintf(buffer, "%s%02d%c%c%04d", 
+        states[rand() % 5],           // Random state code
+        rand() % 100,                 // District code
+        'A' + rand() % 26,           // Random letter
+        'A' + rand() % 26,           // Random letter
+        rand() % 10000               // Random number
+    );
+}       //sprintf is used to format the output and store it in buffer.-------------------------
+
+// Generate a random lane
+char generateLane() {
+    char lanes[] = {'A', 'B', 'C', 'D'};
+    return lanes[rand() % 4];
+}
+
+int main() {
+    int sock;
+    struct sockaddr_in server_address;
+    char buffer[BUFFER_SIZE];
+
+    // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, SERVER_IP, &server_address.sin_addr) <= 0) {
+        perror("Invalid address");
+        exit(EXIT_FAILURE);
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server(simulator)...\n");
+    srand(time(NULL));
+
+    while (1) {
+        char vehicle[9];
+        generateVehicleNumber(vehicle);
+        char lane = generateLane();
+
+        snprintf(buffer, BUFFER_SIZE, "%s:%c", vehicle, lane);
+
+        // Send message
+        send(sock, buffer, strlen(buffer), 0);
+        printf("Sent: %s\n", buffer);
+
+        sleep(1);
+    }
+
+    close(sock);
+    return 0;
+}
